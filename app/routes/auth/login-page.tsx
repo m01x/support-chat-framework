43fec672d74e4b2669af6038aaa09c3a99fb8fbe
m01x <fortiz.informatica@gmail.com>
2025-05-08 08:29:10 -0400

    Enhance login page functionality by integrating session management, updating form handling, and improving user feedback on login errors. Added explicit return for unauthenticated users and adjusted form structure for better data handling.

--- app/routes/auth/login-page.tsx
+++ app/routes/auth/login-page.tsx
@@ -1,4 +1,4 @@
-import { Link, redirect, useNavigate } from "react-router";
+import { data, Form, Link, redirect, useNavigate } from "react-router";
 
 import { Button } from "~/components/ui/button";
 import { Card, CardContent } from "~/components/ui/card";
@@ -7,20 +7,73 @@ import { Input } from "~/components/ui/input";
 
 import placeholder from "~/assets/images/placeholder.svg";
 import type { Route } from "./+types/login-page";
-import { getSession } from "~/sessions.server";
+import { commitSession, getSession } from "~/sessions.server";
 
 
 
-export async function loader({ request }:Route.LoaderArgs){
+export async function loader({ request }: Route.LoaderArgs) {
 
-  const session = await getSession( request.headers.get('Cookie'));
+  const session = await getSession(request.headers.get('Cookie'));
+  console.log('Sesion', session.get('userId'));
 
-  if ( session.get('userId')){
+  if (session.get('userId')) {
     return redirect('/chat');
+  } else {
+    console.log('No hay usuario');
   }
+  /**
+   * En este punto de la clase, todavia no devolvia nada explicitamente, cuando no hay userId
+   * agregando este return, corregí el error. 🍺
+   */
+  //return null;
 
+  return data(
+    { error: session.get('error') },
+    {
+      headers: {
+        'Set-Cookie': await commitSession(session),
+      },
+    }
+  )
 
 }
+
+export async function action({
+  request,
+}: Route.ActionArgs) {
+  const session = await getSession(
+    request.headers.get("Cookie")
+  );
+  const form = await request.formData();
+  const email = form.get("email");
+  const password = form.get("password");
+  
+  if( email === "algo@google.com"){
+    session.flash('error', 'Invalid email');
+    return redirect('/auth/login?error=Invalid Email',{
+      headers:{
+        'Set-Cookie': await commitSession(session),
+      }
+    })
+  }
+
+  
+   session.set('userId', 'U1-12345');
+   session.set('token', 'token-1234567890');
+
+   return redirect('/chat', {
+    headers:{
+      'Set-Cookie': await commitSession(session),
+    }
+   })
+  // // Login succeeded, send them to the home page.
+  // return redirect("/", {
+  //   headers: {
+  //     "Set-Cookie": await commitSession(session),
+  //   },
+  // });
+}
+
 
 
 const LoginPage = () => {
@@ -36,7 +89,8 @@ const LoginPage = () => {
     <div className="flex flex-col gap-6">
       <Card className="overflow-hidden p-0">
         <CardContent className="grid p-0 md:grid-cols-2">
-          <form className="p-6 md:p-8">
+          {/* El name es importantisimo en el Form, para que el campo sea detectado por el FormData*/}
+          <Form method='POST' className="p-6 md:p-8" action="/auth/login">
             <div className="flex flex-col gap-6">
               <div className="flex flex-col items-center text-center">
                 <h1 className="text-2xl font-bold">Welcome back</h1>
@@ -44,7 +98,7 @@ const LoginPage = () => {
               </div>
               <div className="grid gap-2">
                 <Label htmlFor="email">Email</Label>
-                <Input id="email" type="email" placeholder="m@example.com" required />
+                <Input id="email" type="email" placeholder="m@example.com" name="email" required />
               </div>
               <div className="grid gap-2">
                 <div className="flex items-center">
@@ -53,7 +107,7 @@ const LoginPage = () => {
                     Forgot your password?
                   </a>
                 </div>
-                <Input id="password" type="password" required />
+                <Input id="password" type="password" name="password" required />
               </div>
               <Button type="submit" className="w-full">
                 Login
@@ -97,7 +151,7 @@ const LoginPage = () => {
                 </Link>
               </div>
             </div>
-          </form>
+          </Form>
           <div className="relative hidden bg-muted md:block">
             <img
               src={placeholder}
