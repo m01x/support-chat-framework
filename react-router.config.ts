4ea98a47e10e43c400179ba65896982da869dc5c
m01x <fortiz.informatica@gmail.com>
2025-05-14 07:58:54 -0400

    Add prerendering support for specific authentication routes in router configuration. This enhancement allows pre-construction of pages like login and registration for improved performance.

--- react-router.config.ts
+++ react-router.config.ts
@@ -4,4 +4,8 @@ export default {
   // Config options...
   // Server-side render by default, to enable SPA mode set this to `false`
   ssr: true,
+  //Pre-render (ultimas clases del curso), para paginas q no cambian, y quiero pre-construir antes q sean visitadas.
+  async prerender(){
+    return ['/auth/login','/auth/register','auth/testing'];
+  },
 } satisfies Config;
